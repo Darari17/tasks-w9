b950d5fef071a8fd6f9423542a8296205a525494
farid <faridramadhan59@gmail.com>
2025-08-31 19:23:09 +0700

    fix: menghapus query returning pada register query

--- weekly/queries/register.sql
+++ weekly/queries/register.sql
@@ -1,3 +1,2 @@
 INSERT INTO users (email, password, role, created_at, updated_at) 
 VALUES ('email_user', 'password_user', 'user', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) 
-RETURNING id, email, role, created_at, updated_at;
