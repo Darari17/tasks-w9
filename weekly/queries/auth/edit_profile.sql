fd41adcda6182ecdb1d5a561d76ae5a9ee26130a
farid <faridramadhan59@gmail.com>
2025-09-01 09:22:46 +0700

    feat: query-query yang digunakan oleh user

---
+++ weekly/queries/auth/edit_profile.sql
@@ -0,0 +1,3 @@
+UPDATE profile 
+SET firstname = 'firstname_value', lastname = 'lastname_value', phone_number = 'phone_value' 
+WHERE user_id = user_id;
---
+++ weekly/queries/auth/register.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, password, role, created_at) 
+VALUES ('email_user', 'password_user', 'user', CURRENT_TIMESTAMP) 
