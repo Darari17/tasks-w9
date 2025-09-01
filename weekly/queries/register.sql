f745e8c41c480b418fadfd916faf7345ba5d491a
farid <faridramadhan59@gmail.com>
2025-09-01 09:03:48 +0700

    fix: menghilangkan value untuk dimasukkan ke kolom updated_at

--- weekly/queries/register.sql
+++ weekly/queries/register.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (email, password, role, created_at, updated_at) 
-VALUES ('email_user', 'password_user', 'user', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) 
+INSERT INTO users (email, password, role, created_at) 
+VALUES ('email_user', 'password_user', 'user', CURRENT_TIMESTAMP) 
